bb0e6a035024be808dafec73c551004bee6533a2
11249A087 <11249a087@kanchiuniv.ac.in>
2026-09-09 12:39:49 +0530

    Add files via upload

---
+++ SimpleOperatorsDemo.java
@@ -0,0 +1,14 @@
+public class SimpleOperatorsDemo {
+public static void main(String[]args){
+    int a=10,b=3;
+    System.out.println("a+b="+(a+b));
+    System.out.println("a-b="+(a-b));
+    System.out.println("a>b?"+(a>b));
+    boolean x=true,y=false;
+    System.out.println("x&&y="+(x&&y));
+    a+=5;
+    System.out.println("a after+=5:"+a);
+    int max=(a>b)? a:b;
+    System.out.println("Maximum="+max);
+}
+}
