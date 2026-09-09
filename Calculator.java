2d7596b4439e1630e7312f9a18a1c4b277358244
11249A087 <11249a087@kanchiuniv.ac.in>
2026-09-09 12:42:15 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    static int add(int a, int b) {
+        return a + b;
+    }
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static void main(String[] args) {
+        int sum = add(15, 25);
+        int product = multiply(15, 25);
+        System.out.println("Addition=" + sum);
+        System.out.println("Multiplication=" + product);
+    }
+}
