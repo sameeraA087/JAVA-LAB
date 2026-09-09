2210b9178f290be6bb753feca170aeb339c6dfe2
11249A087 <11249a087@kanchiuniv.ac.in>
2026-09-09 12:39:01 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student {
+String name="Rahul";
+int age=20;
+void display(){
+    System.out.println("Name:"+name);
+    System.out.println("Age:"+age);
+}
+public static void main(String[]args){
+    Student s=new Student();
+    s.display();
+}
+}
